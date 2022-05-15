65aca03a0a452044597df74f897ac1ccef2ca579
Noctis Hsu <noctishsu@gmail.com>
2022-05-15 20:42:34 +0800

    feat: Add Apollo client and ChakraProvider to MyApp component entry and remove unused entry file

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "~/src/components/Layout";
+const client = new ApolloClient({
+  uri: "http://localhost:9000/",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <ApolloProvider client={client}>
+        <ChakraProvider>
           <Layout>
             <Component {...pageProps} />
           </Layout>
+        </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
